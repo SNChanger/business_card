b32273bef1e6a08490b40a642ef008482d15d9d1
SNChanger <108204396+SNChanger@users.noreply.github.com>
2022-09-20 03:47:19 +0900

    leafjsの試験導入

--- FrontEndCorner/FirstPage/app.js
+++ FrontEndCorner/FirstPage/app.js
@@ -1,5 +1,36 @@
 'use strict';
 const switcher = document.querySelector('.btn');
+let requestURL = "https://magictravelinterface2030.azurewebsites.net/api/GetMemoryListsForPosition?";
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  const areaMemories = request.response;
+  showMemories(areaMemories);
+}
+
+function showMemories(obj) {
+  const memories = obj['memories'];
+
+  for (let i = 0; i < memories.length; i++) {
+    if (i == 0) {
+      var map = L.map('map').setView([memories[i].startLat + 1, memories[i].startLon +1], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© OpenStreetMap'
+      }).addTo(map);
+
+      var marker = L.marker([memories[i].startLat + 0.995, memories[i].startLon +0.995]).addTo(map);
+
+    }
+    console.log();
+
+  }
+}
+
 
 
 switcher.addEventListener('click', function () {

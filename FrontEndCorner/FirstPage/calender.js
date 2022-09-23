bf5504f726ada3a75b7a57930a0abbf19ed3949d
SNChanger <108204396+SNChanger@users.noreply.github.com>
2022-09-23 13:52:32 +0900

    宇宙カレンダープロトタイプを用意

---
+++ FrontEndCorner/FirstPage/calender.js
@@ -0,0 +1,19 @@
+'use strict';
+DHTMLSuite.include("windowWidget");
+if(location.hostname.toLowerCase().indexOf('dhtmlgoodies') >=0) {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-2042963-3']);
+    _gaq.push(['_trackPageview']);
+  
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
+
+
+function searchCalender(calenderDate) {
+    console.log("跳躍を開始します");
+    console.log(calenderDate.value);
+}
